3db12cba83fef5adfdaab33dddea65e06947695e
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-07-28 11:42:29 +0700

    add types file to manage types and change details into tsx

--- typescript/typescript-1/src/Details.tsx
+++ typescript/typescript-1/src/Details.tsx
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { useContext, useState } from "react";
+import { PetAPIResponse } from "./APIResponsesTypes";
 import AdoptedPetContext from "./AdoptedPetContext";
 import Modal from "./Modal";
 import ErrorBoundary from "./ErrorBoundary";
@@ -9,10 +10,13 @@ import Carousel from "./Carousel";
 
 const Details = () => {
   const { id } = useParams();
+  if (!id) {
+    throw new Error("no id is provided to details");
+  }
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-  const results = useQuery(["details", id], fetchPet);
-  // eslint-disable-next-line no-unused-vars
+  const results = useQuery<PetAPIResponse>(["details", id], fetchPet);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, setAdoptedPet] = useContext(AdoptedPetContext);
 
   if (results.isLoading) {
@@ -23,7 +27,11 @@ const Details = () => {
     );
   }
 
-  const pet = results.data.pets[0];
+  const pet = results?.data?.pets[0];
+
+  if (!pet) {
+    throw new Error("no pet lol");
+  }
 
   return (
     <div className="details">
@@ -56,10 +64,10 @@ const Details = () => {
   );
 };
 
-export default function DetailsErrorBoundary(props) {
+export default function DetailsErrorBoundary() {
   return (
     <ErrorBoundary>
-      <Details {...props} />
+      <Details />
     </ErrorBoundary>
   );
 }
