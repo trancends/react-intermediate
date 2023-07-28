fabeb3c0089280a37125b3d1bacf361eb44c6168
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-07-28 14:05:05 +0700

    complete changing all the components into tsx

---
+++ typescript/typescript-4/src/App.tsx
@@ -0,0 +1,45 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { Pet } from "./APIResponsesTypes";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null as Pet | null);
+  return (
+    <div>
+      <BrowserRouter>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("no container to render to");
+}
+const root = createRoot(container);
+root.render(<App />);
